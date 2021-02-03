242dd45b159bf931dd6123b33aa73236051f70f5
bakial <manhtuan88394@gmail.com>
2021-02-02 21:39:09 -0500

    Add images to buttons

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 for (let index = 0; index < document.querySelectorAll("button").length; index++) {
     document.querySelectorAll("button")[index].addEventListener("click",() => {
-        alert("I got clicked");
+        let audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
     })
     
 }
